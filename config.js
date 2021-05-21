e050f1ad9762e6ec9309419b8bd0db9445163fc7
Sank48 <68440985+Sank48@users.noreply.github.com>
2021-05-21 18:35:56 +0530

    External files

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'1297911197250810',
+        clientSecret:'dcf09bed31013dd129fc39c6ba1b0b6e'
+    }
+}
